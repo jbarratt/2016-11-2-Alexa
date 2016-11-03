f1131d695f48dc67f4da565671907bebc8f837fa
Satchel Grant <grantsrb@gmail.com>
2016-11-02 20:27:08 -0700

    possibly added voice recognition

--- index.js
+++ index.js
@@ -2,53 +2,34 @@
 var alexa = require('alexa-app');
 var app = new alexa.app('Joker');
 
-app.intent('Joke',
-  {
-    'slots':{
-    },
-    'utterances':[
-      "shoot me a joke",
-      "joke time",
-      "tell me joke",
-      "can I hear a joke",
-      "yes"
-    ]
-  },
-  (request, response) => {
-    const jokes = [
-      "What do you call two straight days of rain in Portland?  Normal.",
-      "What is the difference between Oregon and Washington?  Washingtonians have nicer neighbors.",
-      "You might be from Portland if you know a brid and groom that registered at REI"
-    ];
-    var joke =  jokes[Math.floor(Math.random() * jokes.length)];
-    response.say(joke);
-    response.say("Would you like to hear another?");
-  }
-);
+var categories = [""];
 
-app.intent('Done',
-  {
-    'utterances': [
-      "no",
-      "no thankyou",
-      "not now"
-    ]
-  },
-  (request, response) => {
-    response.say("O.K.  Goodbye");
+app.intent('News Help', {
+  'utterances': [
+    "what are the possible headline categories?",
+    "what categories can I try?",
+    "list the categories",
+    "give me the headline categories"
+  ], (request, response) => {
+    response.say("you can say: " + categories.toString());
   }
-);
+});
 
-app.intent('AMAZON.HelpIntent', 
-  {
-    'utterances':[
-      "for help {customizing|setting up|getting started}"
-    ]
+app.intent('Headlines', {
+  'slots':{
+    "name": "Category",
+    "type": categories
   },
-  (request, response) => {
-    
+  'utterances': [
+    "give me headlines for {Category}",
+    "{Category}"
+  ], (request, response) => {
+    response.say("Wassup");
   }
-);
+});
+
+
+
 
 app.error = (exception, request, response) => {
     response.say("Sorry, something bad happened");
